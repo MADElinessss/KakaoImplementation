79590e25fe6a81b2451fc2328558bfcadc26b059
MADElinessss <jyseen@naver.com>
2023-01-11 11:17:36 +0900

    no need to use retrofit\nlogin logic

--- app/src/main/java/com/example/kakaologin5/MainActivity.kt
+++ app/src/main/java/com/example/kakaologin5/MainActivity.kt
@@ -7,9 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.telecom.Call
 import android.util.Log
-import android.view.View
-import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
 import com.example.kakaologin5.databinding.ActivityMainBinding
 import com.kakao.sdk.auth.model.OAuthToken
@@ -42,9 +39,9 @@ class MainActivity : AppCompatActivity() {
         binding.btnLogin.setOnClickListener {
             kakaoLogin() //로그인
         }
-        UserApiClient.instance.me{user, error ->
-            binding.tvNickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}"
-            binding.tvId.text = "닉네임: ${user?.kakaoAccount?.email}"
+        UserApiClient.instance.me { user, error ->
+            binding.tvNickname.text = "닉네임: ${user?.kakaoAccount?.profile?.nickname}\n"
+            binding.tvId.text = "이메일: ${user?.kakaoAccount?.email}\n"
         }
         binding.btnLogout.setOnClickListener {
             kakaoLogout() //로그아웃
@@ -52,25 +49,45 @@ class MainActivity : AppCompatActivity() {
     }
     private fun kakaoLogin() {
 
-        // 카카오톡으로 로그인
-        UserApiClient.instance.loginWithKakaoTalk(this@MainActivity) { token, error ->
-            if (error != null) {
-                Log.e(TAG, "로그인 실패", error)
-            }
-            else if (token != null) {
-                Log.i(TAG, "로그인 성공 ${token.accessToken}")
-            }
-        }
-        
         // 카카오계정으로 로그인 공통 callback 구성
         // 카카오톡으로 로그인 할 수 없어 카카오계정으로 로그인할 경우 사용됨
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
-                Log.e(TAG, "카카오계정으로 로그인 실패", error)
+                when {
+                    error.toString() == AuthErrorCause.AccessDenied.toString() -> {
+                        Toast.makeText(this, "접근이 거부 됨(동의 취소)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidClient.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 앱", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidGrant.toString() -> {
+                        Toast.makeText(this, "인증 수단이 유효하지 않아 인증할 수 없는 상태", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidRequest.toString() -> {
+                        Toast.makeText(this, "요청 파라미터 오류", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.InvalidScope.toString() -> {
+                        Toast.makeText(this, "유효하지 않은 scope ID", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Misconfigured.toString() -> {
+                        Toast.makeText(this, "설정이 올바르지 않음(android key hash)", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.ServerError.toString() -> {
+                        Toast.makeText(this, "서버 내부 에러", Toast.LENGTH_SHORT).show()
+                    }
+                    error.toString() == AuthErrorCause.Unauthorized.toString() -> {
+                        Toast.makeText(this, "앱이 요청 권한이 없음", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> { // Unknown
+                        Toast.makeText(this, "기타 에러", Toast.LENGTH_SHORT).show()
+                    }
+                }
             } else if (token != null) {
                 Log.i(TAG, "카카오계정으로 로그인 성공 ${token.accessToken}")
+                binding.tvAccessToken.text = "access token : \n${token.accessToken}\n"
             }
         }
+
         // 카카오톡이 설치되어 있으면 카카오톡으로 로그인, 아니면 카카오계정으로 로그인
         if (UserApiClient.instance.isKakaoTalkLoginAvailable(this@MainActivity)) {
             UserApiClient.instance.loginWithKakaoTalk(this@MainActivity) { token, error ->
@@ -94,6 +111,17 @@ class MainActivity : AppCompatActivity() {
         } else {
             UserApiClient.instance.loginWithKakaoAccount(this@MainActivity, callback = callback)
         }
+
+        // 카카오톡으로 로그인
+        UserApiClient.instance.loginWithKakaoTalk(this@MainActivity) { token, error ->
+            if (error != null) {
+                Log.e(TAG, "로그인 실패", error)
+            }
+            else if (token != null) {
+                Log.i(TAG, "로그인 성공 ${token.accessToken}")
+
+            }
+        }
     }
     private fun kakaoLogout(){
         // 로그아웃
@@ -101,6 +129,7 @@ class MainActivity : AppCompatActivity() {
             if (error != null) {
                 Toast.makeText(this, "로그아웃 실패 $error", Toast.LENGTH_SHORT).show()
             }else {
+                binding.tvIsloginned.text = "로그아웃 되었습니다."
                 Toast.makeText(this, "로그아웃 성공", Toast.LENGTH_SHORT).show()
             }
         }
